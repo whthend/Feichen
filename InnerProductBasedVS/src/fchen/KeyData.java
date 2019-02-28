a9d5a3d7fa1744ed60c761b120355dd3b6ccbc14
whthend <whthend@outlook.com>
2019-02-28 11:54:53 +0800

    调整格式

--- InnerProductBasedVS/src/fchen/KeyData.java
+++ InnerProductBasedVS/src/fchen/KeyData.java
@@ -39,30 +39,30 @@ public class KeyData
      */
     public KeyData(String keyPRF, String r, int bitLen)
     {
-		// first generate the random finite field
-		Random temp = new Random();
-		this.p = BigInteger.probablePrime(bitLen, temp);
-		this.r = new BigInteger(r);
-		
-		byte[] gTemp = new byte[bitLen / 8];
-		temp.nextBytes(gTemp);
-		this.g = new BigInteger(1, gTemp); 
+			// first generate the random finite field
+			Random temp = new Random();
+			this.p = BigInteger.probablePrime(bitLen, temp);
+			this.r = new BigInteger(r);
 			
-		this.bitLen = bitLen;
+			byte[] gTemp = new byte[bitLen / 8];
+			temp.nextBytes(gTemp);
+			this.g = new BigInteger(1, gTemp); 
+				
+			this.bitLen = bitLen;
 
-		this.seed = new SecureRandom(keyPRF.getBytes());
-		try
-		{
-			this.kg = KeyGenerator.getInstance("HmacSHA256");
-			this.kg.init(this.seed);
-			this.sk = this.kg.generateKey();
-			this.mac = Mac.getInstance("HmacSHA256");
-			this.mac.init(this.sk);
-		} catch (Exception e)
-		{
-			System.out.println("Exception in KeyData: initializing mac: " + e);
-		}
-    }
+			this.seed = new SecureRandom(keyPRF.getBytes());
+			try
+				{
+					this.kg = KeyGenerator.getInstance("HmacSHA256");
+					this.kg.init(this.seed);
+					this.sk = this.kg.generateKey();
+					this.mac = Mac.getInstance("HmacSHA256");
+					this.mac.init(this.sk);
+				} catch (Exception e)
+				{
+					System.out.println("Exception in KeyData: initializing mac: " + e);
+				}
+				}
 
     /**
      * It implements a pseudorandom function using HMAC as the underlying constructing component.
@@ -71,33 +71,33 @@ public class KeyData
      */
     public BigInteger getRandomElement(int index)
     {
-		int length = bitLen / 8;
-		byte[] value = new byte[length];
+			int length = bitLen / 8;
+			byte[] value = new byte[length];
 
-		int loop = (bitLen / 256) * 32;
-		String temp = "";
-		int i = 0;
-		for (i = 0; i < loop; i = i + 32) // 256 bits are 32 bytes;
-		{
-			temp = temp + Integer.toString(index);
-			this.mac.update(temp.getBytes());
-			try{
-				this.mac.doFinal(value, i);
+			int loop = (bitLen / 256) * 32;
+			String temp = "";
+			int i = 0;
+			for (i = 0; i < loop; i = i + 32) // 256 bits are 32 bytes;
+			{
+				temp = temp + Integer.toString(index);
+				this.mac.update(temp.getBytes());
+				try{
+					this.mac.doFinal(value, i);
+				}
+				catch (Exception e){
+					System.out.println("Exception when generating a random element: " + e);
+				}
 			}
-			catch (Exception e){
-				System.out.println("Exception when generating a random element: " + e);
+			
+			if (loop % 256 != 0)
+			{
+				temp = temp + Integer.toString(index);
+				byte[] tempMac = this.mac.doFinal(temp.getBytes());
+				for (int j = i; j < value.length; j++)
+				value[j] = tempMac[j - i];
 			}
-		}
-		
-		if (loop % 256 != 0)
-		{
-			temp = temp + Integer.toString(index);
-			byte[] tempMac = this.mac.doFinal(temp.getBytes());
-			for (int j = i; j < value.length; j++)
-			value[j] = tempMac[j - i];
-		}
 
-		return new BigInteger(1, value);
+			return new BigInteger(1, value);
     }
 
     /**
@@ -105,10 +105,10 @@ public class KeyData
      */
     public void print()
     {
-	System.out.println("The system key is as follows:");
-	System.out.println("p: " + p.toString());
-	System.out.println("g: " + g.toString());
-	System.out.println("r: " + r.toString());
-	System.out.println("block size: " + bitLen);
+			System.out.println("The system key is as follows:");
+			System.out.println("p: " + p.toString());
+			System.out.println("g: " + g.toString());
+			System.out.println("r: " + r.toString());
+			System.out.println("block size: " + bitLen);
     }
 }
